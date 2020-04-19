b6e840f6b772b0ef912d09b0080165530880e119
Jason McDermott <jmcdermott@rightpoint.com>
2020-04-19 08:54:17 -0400

    refactor stateless functional components to implicitly return JSX

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,14 +1,12 @@
 import React from 'react'
 
-const Option = (props) => {
-    return (
-        <div>
-            <li>{props.item}</li>
-            <button onClick={(e) => {
-                props.handleDeleteOption(props.item);
-            }}>remove</button>
-        </div>
-    );
-};
+const Option = (props) => (
+    <div>
+        <li>{props.item}</li>
+        <button onClick={(e) => {
+            props.handleDeleteOption(props.item);
+        }}>remove</button>
+    </div>
+);
 
 export default Option
--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -3,9 +3,9 @@ import Modal from 'react-modal'
 
 const OptionModal = (props) => (
     <Modal 
-    isOpen={!!props.selectedOption} 
-    onRequestClose={props.handleClearSelectedOption}
-    contentLabel="Selected Option">
+        isOpen={!!props.selectedOption} 
+        onRequestClose={props.handleClearSelectedOption}
+        contentLabel="Selected Option">
         <h3>Selected Option</h3>
         {props.selectedOption && <p>{props.selectedOption}</p>}
         <button onClick={props.handleClearSelectedOption}>OK</button>
